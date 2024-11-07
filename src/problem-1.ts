6fe9a78ee1a1ca4405f22aa6613a23f4d97e5740
freelancersifat627e <freelancersifat627@gmail.comm>
2024-11-07 14:33:02 +0600

    solve probleam-1

--- src/problem-1.ts
+++ src/problem-1.ts
@@ -1,1 +1,17 @@
-// console.log('Horrayyyyyyyyyyyyyyyyyyyyyyyyyy');
+{
+    //////////////////////////////// Start////////////////////////////////
+
+    // Write a TypeScript function sumArray that takes an array of numbers and returns the sum of all elements in the array.
+
+    const sumArray = (num : number[]) : number => {
+        const sum = num?.reduce((accumulator, currentValue) : number => accumulator + currentValue , 0);
+        return sum ;
+    };
+
+    console.log(sumArray([1, 2, 3, 4, 5]));
+
+
+
+
+    //////////////////////////////// End////////////////////////////////
+}
