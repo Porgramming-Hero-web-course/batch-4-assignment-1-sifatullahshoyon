cb85d107fd4293290581ed17639a3474f6498fb3
freelancersifat627e <freelancersifat627@gmail.comm>
2024-11-08 17:49:17 +0600

    sove probleam 4

--- src/problem-4.ts
+++ src/problem-4.ts
@@ -1,21 +1,44 @@
 {
-    //////////////////////////////// Start////////////////////////////////
+  //////////////////////////////// Start////////////////////////////////
 
-    // Define a union type Circle and Rectangle, where each type has a unique shape property. Create a function calculateShapeArea that uses type guards to calculate the area based on the shape type.
+  // Define a union type Circle and Rectangle, where each type has a unique shape property. Create a function calculateShapeArea that uses type guards to calculate the area based on the shape type.
 
-    type Tshape = {
-        shape : string;
-        width: number;
-        height: number;
-    };
+  // Type alias
+  type Circle = {
+    shape: "circle";
+    radius: number;
+  };
 
-    type TshapeProperty = keyof Tshape;
+  type Rectangle = {
+    shape: "rectangle";
+    width: number;
+    height: number;
+  };
 
-    // const calculateShapeArea = (shape : 'Circle' | 'Rectangle' , width : number , height : number) : number => {};
-    const calculateShapeArea = (shape : 'Circle' | 'Rectangle' , width : number , height : number) : number => {};
+  // Union type
+  type Shape = Circle | Rectangle;
 
-   
-    
+  function calculateShapeArea(shape: Shape): number {
+    if (shape.shape === "circle") {
+      const circleShape = Math.PI * shape.radius * shape.radius;
+      return parseFloat(circleShape.toFixed(2));
+    } else if (shape.shape === "rectangle") {
+      return shape.width * shape.height;
+    }
+    return 0;
+  }
 
-    //////////////////////////////// End////////////////////////////////
+  //  Input 1
+  const circleArea = calculateShapeArea({ shape: "circle", radius: 5 });
+  //   console.log(circleArea);
+
+  //  Input 2
+  const rectangleArea = calculateShapeArea({
+    shape: "rectangle",
+    width: 4,
+    height: 6,
+  });
+  //   console.log(rectangleArea);
+
+  //////////////////////////////// End////////////////////////////////
 }
